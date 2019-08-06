82fbd82ade45d7a1abe0b9994cb177ace0fa30c1
veronikh <veronikh@netapp.com>
2019-08-06 13:25:24 +0200

    sound.py with logging

--- sound.py
+++ sound.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python
 import RPi.GPIO as GPIO
 import time
+import logging
+import datetime
+
+logging.basicConfig(level=logging.INFO)
 
 #GPIO SETUP
 channel = 17
@@ -10,8 +14,11 @@ GPIO.setup(channel, GPIO.IN)
 def callback(channel):
         if GPIO.input(channel):
                 print("Sound Detected!")
+                logging.info(datetime.datetime.now().time())
+                print(GPIO.input(channel))
         else:
                 print("Sound Detected!")
+                logging.info(datetime.datetime.now().time())
 
 GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)  # let us know when the pin goes HIGH or LOW
 GPIO.add_event_callback(channel, callback)  # assign function to GPIO PIN, Run function on change
